48de02bce3690024691a35d1e72b7f19fb8fe778
daniel.stancl@gmail.com <stancld@Danko-Notebok.localdomain>
2020-06-30 15:31:59 +0200

    scraping.py in wikiscraper changed

--- Wikipedia scraping/scraping.py
+++ Wikipedia scraping/scraping.py
@@ -5,22 +5,19 @@ from django.utils import timezone
 from WikiScraper import *
 
 if __name__=='__main__':
-    pass
+    # import Company - django.db.models.Model
+    exec(open('set_django_db.py').read())
+    from tables_daniel.models import Company
 
-# import Company - django.db.models.Model
-exec(open('set_django_db.py').read())
-print(sys.path)
-from tables_daniel.models import Company
+    # parameters
+    stock_indices = ['S&P 500', 'FTSE 100', 'EURO STOXX 50']
+    stock_indices = ['EURO STOXX 50']
 
-# parameters
-stock_indices = ['S&P 500', 'FTSE 100', 'EURO STOXX 50']
-stock_indices = ['EURO STOXX 50']
-
-#######################
-##### APPLICATION #####
-#######################
-wikiScraper = WikiScraper(CompanyWriter=Company)
-for stock_index in stock_indices:
-    wikiScraper.scrapeWikipedia(stock_index)
-wikiScraper.scrapeYahooFinance()
-wikiScraper.writeToDjangoDB()
+    #######################
+    ##### APPLICATION #####
+    #######################
+    wikiScraper = WikiScraper(CompanyWriter=Company)
+    for stock_index in stock_indices:
+        wikiScraper.scrapeWikipedia(stock_index)
+    wikiScraper.scrapeYahooFinance()
+    wikiScraper.writeToDjangoDB()
